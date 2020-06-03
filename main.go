bcd26ac0d81cca1ec37b1d7da30256eba2d6cf2d
miaozilong <miao.zilong@qq.com>
2020-06-04 00:47:42 +0800

    使用协程处理多任务,第一次使用协程的等待

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"os"
+	"sync"
+	"time"
 )
 
+var wg sync.WaitGroup //定义一个同步等待的组
+
 func main() {
+	startTime := time.Now()
 	f, err := excelize.OpenFile("base64.xlsx")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	const PICPATH = "pics"
+	os.Mkdir(PICPATH, os.ModePerm)
 	rows := f.GetRows("Sheet1")
 	for i, v := range rows {
 		if i == 0 {
@@ -21,9 +28,18 @@ func main() {
 		base64code := v[2]
 		filename := v[0] + "." + v[1]
 		dist, _ := base64.StdEncoding.DecodeString(base64code)
-		//写入新文件
-		f, _ := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
-		f.Write(dist)
-		f.Close()
+		wg.Add(1)
+		go writePic(PICPATH, filename, dist)
 	}
+	wg.Wait()
+	elapsed := time.Since(startTime)
+	fmt.Println(elapsed)
+}
+
+func writePic(PICPATH string, filename string, dist []byte) {
+	//写入新文件
+	f, _ := os.OpenFile(PICPATH+"/"+filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	f.Write(dist)
+	f.Close()
+	wg.Done()
 }
